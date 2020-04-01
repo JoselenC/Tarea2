02e1b36b42da40955159fd622d40fbdc189859be
JoselenC <choche_cr@hotmail.com>
2020-03-31 21:04:58 -0300

    A

---
+++ Usuario.java
@@ -0,0 +1,12 @@
+
+public class Usuario {
+
+     private String nombre;
+     private int cedula;
+     private int contraseña;
+     private int edad;
+     private int cantHijos;
+     private int saldo=0;
+
+   
+}
